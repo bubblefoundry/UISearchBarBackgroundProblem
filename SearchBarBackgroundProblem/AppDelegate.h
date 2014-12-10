71c06003343c01d29fff0a40ff6073df69d3f4b0
Peter Robinett <peter@getlua.com>
2014-12-10 17:51:18 -0500

    Initial Commit

---
+++ SearchBarBackgroundProblem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBarBackgroundProblem
+//
+//  Created by Peter Robinett on 12/10/14.
+//  Copyright (c) 2014 Bubble Foundry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchBarBackgroundProblemTests/SearchBarBackgroundProblemTests.m
@@ -0,0 +1,40 @@
+//
+//  SearchBarBackgroundProblemTests.m
+//  SearchBarBackgroundProblemTests
+//
+//  Created by Peter Robinett on 12/10/14.
+//  Copyright (c) 2014 Bubble Foundry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SearchBarBackgroundProblemTests : XCTestCase
+
+@end
+
+@implementation SearchBarBackgroundProblemTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
